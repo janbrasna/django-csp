462e3f054911aa8b00449552beead9f4f786d0ff
Jan Brasna <1784648+janbrasna@users.noreply.github.com>
2025-01-20 12:26:13 +0100

    Comment conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,12 @@ from typing import Dict
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-# needs_sphinx = '1.0'
+# needs_sphinx = '8.1'  # (8.1 to support %Y in copyright)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    "sphinx.ext.autodoc",
+    # "sphinx.ext.autodoc",
     "sphinx_rtd_theme",
 ]
 
